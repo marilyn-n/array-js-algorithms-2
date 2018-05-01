0612613cd4c77df9646d76c3bce8eaa4199355ba
Marilyn Negrete <negrete.aguilar.m@gmail.com>
2018-04-30 21:32:03 -0500

    frequentItem completed

--- main.js
+++ main.js
@@ -20,16 +20,28 @@ const arrayToString = (array) => {
 };
 
 // TEST
-console.log(arrayToString(['Red', 'Green', 'White', 'Black']));
-console.log(arrayToString(['Red', 'Green', 'White', 'Black'].join('+')));
+const myColor = ['Red', 'Green', 'White', 'Black', 'Violet'];
+console.log(arrayToString(myColor));
+console.log(arrayToString(myColor.join('+')));
 
-// 3. Write a JavaScript program which accept a number as
-// input and insert dashes (-) between each two even numbers.
-// For example if you accept 025468 the output should be 0-254-6-8.
-
-// 4. Write a JavaScript program to find the most frequent item of an array. Go to the editor
+// 3. Write a JavaScript program to find the most frequent item of an array. Go to the editor
 // Sample array : var arr1=[3, 'a', 'a', 'a', 2, 3, 'a', 3, 'a', 2, 4, 9, 3];
 // Sample Output : a ( 5 times )
+
+const frequentItem = (arr) => {
+  const counts = {};
+  for (const i in arr) {
+    const num = arr[i];
+    counts[num] = counts[num] ? counts[num] + 1 : 1;
+  }
+  return counts;
+};
+
+console.log(frequentItem([3, 'a', 'a', 'a', 2, 3, 'a', 3, 'a', 2, 4, 9, 3]));
+
+// 4. Write a JavaScript program which accept a number as
+// input and insert dashes (-) between each two even numbers.
+// For example if you accept 025468 the output should be 0-254-6-8.
 
 // 5. Write a JavaScript program which prints the elements of the following array. Go to the editor
 // Note : Use nested for loops.

6a5a21f7fce6dc95bda08522df99b14463198f62
Marilyn Negrete <negrete.aguilar.m@gmail.com>
2018-05-04 15:29:23 -0500

    objects.js initialized

---
+++ object.js
@@ -0,0 +1,89 @@
+// 1. Write a JavaScript program to list the properties of a JavaScript object. Go to the editor
+// Sample object:
+// var student = {
+// name : "David Rayy",
+// sclass : "VI",
+// rollno : 12 };
+// Sample Output: name,sclass,rollno
+
+
+// 2. Write a JavaScript program to delete the rollno property from the following object.
+// Also print the object before or after deleting the property. Go to the editor
+// Sample object:
+// var student = {
+// name : "David Rayy",
+// sclass : "VI",
+// rollno : 12 };
+
+// 3. Write a JavaScript program to display the reading status
+// (i.e. display book name, author name and reading status) of the following books.
+
+// var library = [
+//    {
+//        author: 'Bill Gates',
+//        title: 'The Road Ahead',
+//        readingStatus: true
+//    },
+//    {
+//        author: 'Steve Jobs',
+//        title: 'Walter Isaacson',
+//        readingStatus: true
+//    },
+//    {
+//        author: 'Suzanne Collins',
+//        title:  'Mockingjay: The Final Book of The Hunger Games',
+//        readingStatus: false
+//    }];
+
+// 4. Write a JavaScript program to create a Clock.
+// Note: The output will come every second.
+// Expected Console Output :
+// "14:37:42"
+// "14:37:43"
+// "14:37:44"
+// "14:37:45"
+// "14:37:46"
+// "14:37:47"
+
+// 5 .Write a JavaScript program to sort an array of JavaScript objects. Go to the editor
+// Sample Object :
+
+// var library = [
+//    {
+//        title:  'The Road Ahead',
+//        author: 'Bill Gates',
+//        libraryID: 1254
+//    },
+//    {
+//        title: 'Walter Isaacson',
+//        author: 'Steve Jobs',
+//        libraryID: 4264
+//    },
+//    {
+//        title: 'Mockingjay: The Final Book of The Hunger Games',
+//        author: 'Suzanne Collins',
+//        libraryID: 3245
+//    }];
+
+//    Expected Output:
+
+// [[object Object] {
+//   author: "Walter Isaacson",
+//   libraryID: 4264,
+//   title: "Steve Jobs"
+// }, [object Object] {
+//   author: "Suzanne Collins",
+//   libraryID: 3245,
+//   title: "Mockingjay: The Final Book of The Hunger Games"
+// }, [object Object] {
+//   author: "The Road Ahead",
+//   libraryID: 1254,
+//   title: "Bill Gates"
+// }]
+
+
+// Bonus================
+
+//  Write a JavaScript function to check whether a given value is a DOM element.
+
+//  Write a JavaScript function to check if an object contains given property.

a5cf9ab26f70731e64a40e3d1ff379b3c17b2538
Marilyn Negrete <negrete.aguilar.m@gmail.com>
2018-05-05 17:29:43 -0500

    objKeys completed

--- object.js
+++ object.js
@@ -1,10 +1,14 @@
-// 1. Write a JavaScript program to list the properties of a JavaScript object. Go to the editor
+// 1. Write a JavaScript program to list the properties of a JavaScript object.
 // Sample object:
-// var student = {
-// name : "David Rayy",
-// sclass : "VI",
-// rollno : 12 };
+const student = {
+  name: 'David Rayy',
+  sclass: 'VI',
+  rollno: 12,
+};
 // Sample Output: name,sclass,rollno
+
+const objKeys = () => Object.keys(student);
+console.log(objKeys().join(','));
 
 
 // 2. Write a JavaScript program to delete the rollno property from the following object.
